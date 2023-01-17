b62c4ce669b633638978949b170181d69910762a
pabloGm1204 <pabloGm1204@gmail.com>
2023-01-17 17:22:47 +0100

    Ejercicio1

---
+++ Ejercicio1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Ejercicio1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int max = Integer.MIN_VALUE;
+        
+        for(int i = 0; i<7; i++){
+            int n = 0;
+            boolean bien = false;
+            do{
+                try{
+                    System.out.print("Dime números: ");
+                    n = Integer.parseInt(sc.nextLine());
+                    bien = true;
+                }catch(Exception e){
+                    System.out.print("Debe escribir un número entero.");
+                    System.out.println("Intentalo de nuevo");
+                }
+            }while(!bien);
+            if(max<n)
+                max = n;
+        }
+        System.out.println("El número mas grande es: "+max);
+        sc.close();
+    }
+}
